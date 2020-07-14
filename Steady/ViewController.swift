ddb2a4085cfd2e34d5ed149f18c2673250bb4f1f
Lauren Moran <laurenhdm@gmail.com>
2020-07-14 14:52:29 -0700

    iteration 1

---
+++ Steady/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Steady
+//
+//  Created by Lauren Moran on 7/13/20.
+//  Copyright © 2020 Lauren Moran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
