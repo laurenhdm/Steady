ecbef9302a38b74f6d2168fb4d739017df45d3a0
Lauren Moran <laurenhdm@gmail.com>
2020-07-15 12:12:22 -0700

    iteration 2

---
+++ Steady/AddEvent.swift
@@ -0,0 +1,16 @@
+//
+//  AddEvent.swift
+//  Steady
+//
+//  Created by Lauren Moran on 7/15/20.
+//  Copyright © 2020 Lauren Moran. All rights reserved.
+//
+
+import UIKit
+
+class Event {
+    var eventName = ""
+    var eventCategory = ""
+    var eventTime = ""
+    var eventMindful = true
+}
---
+++ Steady/AddNewEventViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddNewEventViewController.swift
+//  Steady
+//
+//  Created by Lauren Moran on 7/15/20.
+//  Copyright © 2020 Lauren Moran. All rights reserved.
+//
+
+import UIKit
+
+func createEvent () -> [Event] {
+    let homework = Event()
+    homework.eventName = "English Homework"
+    homework.eventCategory = ""
+}
+
+class AddNewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
