a3503f02ed200f03897046f4c6c80bb316f2fede
Lauren Moran <laurenhdm@gmail.com>
2020-07-17 11:41:01 -0700

    its 11:40am on 7/17/20

--- Steady/ViewController.swift
+++ Steady/ViewController.swift
@@ -10,20 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func feelingYammy(_ sender: UIButton) {
+    @IBAction func showMessage(sender: UIButton) {
+        
+        let emojis = ["✨": "Feeling Zesty!", "🧃": "Feeling Mindful!", "🥀": "Feeling hopeless...", "🥌": "Feeling Motivated!", "🍠": "Feeling YAMMY, BABY!!"]
+        
+        let customMessages = ["Feeling Zesty!": ["As you should!!", "Let's use that beautiful energy for good!"], "Feeling Mindful!": ["Good; take some time for yourself!", "Use this time to take a break, and let go of any lingering stress.", "May the odds be ever in your favor ;)"], "Feeling hopeless...": ["THIS IS WHERE LILA HELPS"], "Feeling Motivated!": ["You've got this!!!", "You're going to get through it, I promise.", "You are creative and loved and wonderful."], "Feeling YAMMY, BABY!!": ["yeah<3", "your mom haha", "are you REALLY alright?", "You're more than enough."]]
+        
+        let selectedEmotion = sender.titleLabel?.text
+        let options = customMessages[emojis[selectedEmotion]!]!
+        let emojiMessage = options[0]// here I want to extract the word that goes with my emoji
+       // then find that word in the dictionary
+       // get it's value (array)
+       // and access the first element (for now)
+        
+        let alertController = UIAlertController()
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+        
+        
+        
     }
     
-    @IBAction func feelingFocused(_ sender: UIButton) {
-    }
     
-    @IBAction func feelingHopeless(_ sender: UIButton) {
-    }
-    
-    @IBAction func feelingMindful(_ sender: UIButton) {
-    }
-    
-    @IBAction func feelingZesty(_ sender: UIButton) {
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
